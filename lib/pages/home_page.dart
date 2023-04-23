0729041dda3a6c76f9222bcd8be5ad7c8d05c922
Harsh Gupta <harsh.gupta2_cs21@gla.ac.in>
2023-04-23 18:30:53 +0530

    Nine day Completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,21 +1,28 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/models/catalog.dart';
 
 import '../widgets/drawer.dart';
+import '../widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
   final int days = 30;
   final String name = "Vrindavan";
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(50, (index) => CaltalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
-        title: Text("Medical App"),
+        title: Text("Stroke Connect Program"),
       ),
-      body: Center(
-        child: Container(
-          child: Text("Welcome to $days days of flutter by $name"),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: CaltalogModel.items.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummyList[index],
+            );
+          },
         ),
       ),
       drawer: MyDrawer(),
